38f61cbb5685701fae81ff83fa9da95617504013
jkru <julie.hollek@gmail.com>
2014-10-21 00:45:46 -0700

    initial commit

---
+++ cookroulette.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, session, render_template, g, redirect, url_for, flash
+import jinja2
+import os
+import random
+import recipe_classes
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/whee")
+def whee():
+    recipe_kinds = {"Quiche" : recipe_classes.Quiche(),"Gratin" : recipe_classes.Gratin(),"Risotto" : recipe_classes.Risotto()}
+    recipe_struct = random.choice(["Quiche","Gratin","Risotto"])
+    
+    this_recipe = recipe_kinds[recipe_struct]
+    
+
+    html = render_template("whee.html", recipe = recipe_struct, specific_struct_prep = )
+    return html
+
+def populate_recipe(recipe_struct):
+    recipe_dict = {}
+
+    return recipe_dict
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ recipe_classes.py
@@ -0,0 +1,22 @@
+#create a class that has a recipe thing
+class Recipe(object):
+    def __init__(self, vegetable, cheese, meat):
+    self.vegetable = "green beans"
+    self.cheese = "Parmesan"
+    self.meat = "bacon"
+
+
+#look up inheritance crap
+class Quiche(Recipe):
+    def __init__(self)
+class Risotto(Recipe):
+
+class Gratin(Recipe):
+
+
+#something like recipe_skeleton
+#attributes like main_element
+#attributes like vegetable
+#attributes like meat
+
+#create subclasses
